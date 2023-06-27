3f14787946ce931440ecc4fdfdc953c466c8ca78
Sudip Ghimire <ghimiresdp@gmail.com>
2023-06-27 06:05:41 +0545

    fix: q0303

--- Quiz Chapter 3/Q0303.py
+++ Quiz Chapter 3/Q0303.py
@@ -23,12 +23,41 @@ print(len(numbers))     #3
 numbers.append([40, 61, 10])
 print(numbers)          # [[12, 52, 37], [46, 51, 16], [17, 82, 39], [40, 61, 10]]
 for item in numbers:
-    print(f'The item is {item}') 
+    print(f'The item is {item}')
 """
 The item is [12, 52, 37]
 The item is [46, 51, 16]
 The item is [17, 82, 39]
 The item is [40, 61, 10]
-"""  
+"""
+
+# todo: review [added by ghimiresdp]
+
+# Bonus: Try using nested foreach to access each item inside of the inner list
+for outer_item in numbers:
+    for inner_item in outer_item:
+        print("The item is:", inner_item)
+"""
+The item is: 12
+The item is: 52
+The item is: 37
+The item is: 46
+The item is: 51
+The item is: 16
+The item is: 17
+The item is: 82
+The item is: 39
+The item is: 40
+The item is: 61
+The item is: 10
+"""
+
+# Bonus: Try finding out the length of each inner list
+for outer_item in numbers:
+    print("the length of the inner item is: ", len(outer_item))
+
+# Finally clear the list multi using the clear() method and verify if the list is empty or not.
+
+
 numbers.clear()
 print(numbers)          # []
